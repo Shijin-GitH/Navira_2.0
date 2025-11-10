54e6e8f40900025424aa5dd51638202ea165cff6
SHIJIN ABRAHAM <shijinabraham2003@gmail.com>
2025-11-10 21:06:05 +0530

    setup: add initial folder structure, install tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <div className="h-screen w-screen overflow-y-auto flex flex-col gap-6 items-center justify-center overflow-x-hidden bg-linear-to-tr from-primary to-black text-secondary font-primary">
+      <h1 className='text-9xl'>NAVIRA 2.0</h1>
+    </div>
+  )
+}
+
+export default App
