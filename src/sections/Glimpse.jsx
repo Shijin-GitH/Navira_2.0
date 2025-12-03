ce760cbd3a96e895945e954eaf511eb89427a44c
Shijin <shijinabraham2003@gmail.com>
2025-12-03 19:09:05 +0530

    update: add images

--- src/sections/Glimpse.jsx
+++ src/sections/Glimpse.jsx
@@ -8,34 +8,44 @@ export default function Glimpse() {
       <div className="flex flex-col ">
         <Marquee>
           <img
-            src="/assets/images/navira.jpeg"
+            src="/assets/images/img1.jpg"
             alt=""
             className="w-50 sm:w-80 md:w-100 rounded-xl"
           />
           <img
-            src="/assets/images/navira2.jpeg"
+            src="/assets/images/img2.jpg"
             alt=""
             className="w-50 sm:w-80 md:w-100 rounded-xl"
           />
           <img
-            src="/assets/images/navira2.jpeg"
+            src="/assets/images/img3.jpg"
+            alt=""
+            className="w-50 sm:w-80 md:w-100 rounded-xl"
+          />
+          <img
+            src="/assets/images/img4.jpg"
             alt=""
             className="w-50 sm:w-80 md:w-100 rounded-xl"
           />
         </Marquee>
         <Marquee reverse={true}>
           <img
-            src="/assets/images/navira.jpeg"
+            src="/assets/images/img5.jpg"
             alt=""
             className="w-50 sm:w-80 md:w-100 rounded-xl"
           />
           <img
-            src="/assets/images/navira2.jpeg"
+            src="/assets/images/img6.jpg"
             alt=""
             className="w-50 sm:w-80 md:w-100 rounded-xl"
           />
           <img
-            src="/assets/images/navira2.jpeg"
+            src="/assets/images/img7.jpg"
+            alt=""
+            className="w-50 sm:w-80 md:w-100 rounded-xl"
+          />
+          <img
+            src="/assets/images/img8.jpg"
             alt=""
             className="w-50 sm:w-80 md:w-100 rounded-xl"
           />
